bb36815d07dbea0326bb25443e8625b302dd2910
Ali <101870797+alitarakci2@users.noreply.github.com>
2023-05-03 00:45:34 +0300

    adding get endpoints for places and managing errors

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -1,10 +1,49 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
 
-router.get('/', (req,res,next) => {
-    console.log('GET Request in places');
-    res.json({message: 'It woks!'});
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Empire state building",
+    description: "One of the most famous skyscrappers in the world",
+    location: {
+      lat: 40.765743634,
+      lng: -73.984435435,
+    },
+    adress: "20 W 34th St, New York, NY 10001",
+    creator: "u1",
+  },
+];
+
+router.get("/:pid", (req, res, next) => {
+  const placeId = req.params.pid;
+  const place = DUMMY_PLACES.find((p) => {
+    return p.id === placeId;
+  });
+
+  if (!place) {
+    const error = new Error("Could not find a place for the provided id.");
+    error.code = 404;
+   return next(error);
+  }
+  res.json({ place: place });
+});
+
+router.get("/user/:uid", (req, res, next) => {
+  const userId = req.params.uid;
+
+  const place = DUMMY_PLACES.find((p) => {
+    return p.creator === userId;
+  });
+
+  if (!place) {
+    const error = new Error("Could not find a place for the provided User id.");
+    error.code = 404;
+   return next(error);
+  }
+
+  res.json({ place });
 });
 
 module.exports = router;
